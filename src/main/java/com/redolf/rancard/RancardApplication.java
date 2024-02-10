b80e0b0c133a26d3340be3f749113270b71ad5ee
redolf250 <55606159+redolf250@users.noreply.github.com>
2024-02-10 19:16:58 +0000

    no changes

--- src/main/java/com/redolf/rancard/RancardApplication.java
+++ src/main/java/com/redolf/rancard/RancardApplication.java
@@ -2,10 +2,10 @@ package com.redolf.rancard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.datasource.AbstractDataSource;
 
 @SpringBootApplication
 public class RancardApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(RancardApplication.class, args);
     }
